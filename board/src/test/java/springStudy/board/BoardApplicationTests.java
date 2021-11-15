4085e935f18678469805c92beef8005cf927bdec
xi_jjun <rlawowns97@naver.com>
2021-11-16 01:26:46 +0900

    reset

--- board/src/test/java/springStudy/board/BoardApplicationTests.java
+++ board/src/test/java/springStudy/board/BoardApplicationTests.java
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import springStudy.board.domain.Board;
-import springStudy.board.domain.User;
-import springStudy.board.domain.enums.BoardType;
+
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -15,84 +13,10 @@ import java.util.List;
 @SpringBootTest
 @Transactional
 class BoardApplicationTests {
-	@Autowired
-	private EntityManager em;
-
-	@Autowired private BoardRepository boardRepository;
-	@Autowired private UserRepository userRepository;
 
 	@Test
-	void insert() { // insert into User (id, name...) values (1L, ...)
-		// 새로운 회원가입
-		User user = new User("재준", "공돌이", "1234pw",
-				"rrl@gmail.com");
+	void insert() {
 
-		Board newContent = new Board("제목", "부제목","내용이다ㅏㅏㅏㅏㅏㅏㅏ",
-				BoardType.FREE, user);
-
-		boardRepository.save(newContent);
-
-		Board findBoard = boardRepository.findById(newContent.getIdx());
-		Assertions.assertThat(findBoard).isEqualTo(newContent);
-	}
-
-	@Test
-	void findBoardByUserNickName() {
-		User user = new User("UserA1", "공돌이1", "1234pw",
-				"rrl@gmail.com");
-
-		Board newContent = new Board("제목A", "A부제목","A내용이다ㅏㅏㅏㅏㅏㅏㅏ",
-				BoardType.FREE, user);
-
-		Board newContent1 = new Board("제1목", "부제목","내용이다ㅏㅏㅏㅏㅏㅏㅏ",
-				BoardType.FREE, user);
-
-		Board newContent2 = new Board("제목", "부제목","내용이다ㅏㅏㅏㅏㅏㅏㅏ",
-				BoardType.FREE, user);
-
-		em.persist(user);
-		boardRepository.save(newContent);
-		boardRepository.save(newContent1);
-		boardRepository.save(newContent2);
-
-		em.flush();
-		em.clear();
-//		List<Board> thisUserContents = boardRepository.findAllContentsByUser(user.getNickName());
-//		for (Board content : thisUserContents) {
-//			System.out.println("content = " + content);
-//		}
-//		Assertions.assertThat(3).isEqualTo(thisUserContents.size());
-	}
-
-	@Test
-	void saveUser() {
-		// Join User
-		User user = new User("UserB", "공돌이1B", "1234pw",
-				"rrl@gmail.com");
-		userRepository.save(user);
-
-		// find User
-		User findUser = userRepository.findOne(user.getIdx());
-
-		// check ? JoinUser == Find User
-		Assertions.assertThat(user).isEqualTo(findUser);
-	}
-
-	@Test
-	void saveAndFindUsers() {
-		User user1 = new User("User11", "공돌이1111", "1234pw",
-				"rrl@gmail.com");
-		User user2 = new User("User22", "공돌이2222", "1234pw",
-				"rrl@gmail.com");
-
-		userRepository.save(user1);
-		userRepository.save(user2);
-
-		List<User> findUsers = userRepository.findByName("User11");
-		for (User findUser : findUsers) {
-			System.out.println("findUser = " + findUser.getName() + " " + findUser.getNickName());
-		}
-		Assertions.assertThat(findUsers.size()).isEqualTo(1);
 	}
 
 }
